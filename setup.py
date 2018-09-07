44785d00f2a972c64a8fb4fd6c909cc07b69d5c2
mscheremetjew <maxim@ebi.ac.uk>
2018-09-07 11:52:47 +0100

    Initial set up (in progress).

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(name='fetch_tool',
+      version='0.1',
+      description='Tool which allows you to fetch RAW read files from the '
+                  'European Nucleotide Archive (ENA).',
+      url='https://github.com/EBI-Metagenomics/fetch_tool',
+      author='Maxim Scheremetjew',
+      license='Apache Software License',
+      packages=find_packages(exclude=['ez_setup']),
+      zip_safe=False,
+      python_requires=">=3.4",)
