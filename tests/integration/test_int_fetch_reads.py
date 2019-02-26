df66b205869a5a0249f17d182068a5486684ecbc
Miguel Boland <migueldboland@gmail.com>
2019-02-26 13:54:18 +0000

    Disabled integration tests on travisCI due to slow ftp speeds

--- tests/integration/test_int_fetch_reads.py
+++ tests/integration/test_int_fetch_reads.py
@@ -84,6 +84,7 @@ def validate_single_study_run(tmpdir):
         assert os.path.getsize(f_path) > 0
 
 
+@pytest.mark.skipif(os.environ['TRAVIS'] == 'true', reason='Skipped as running on TravisCI')
 class TestFetchCompleteStudyReads:
     def test_fetch_all_study_data(self, tmpdir):
         with WorkingDir(tmpdir):
